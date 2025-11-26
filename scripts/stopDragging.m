5a935f3cdc13024b51d49977e044698f367a232d
George R Heath <90329395+George-R-Heath@users.noreply.github.com>
2025-11-26 14:26:59 +0000

    Delete scripts directory

--- scripts/stopDragging.m
+++
@@ -1,6 +0,0 @@
-    function stopDragging(app, ~, ~)
-        fig = ancestor(app.Hist_Axes, 'figure');
-        set(fig, 'WindowButtonMotionFcn', '');
-        set(fig, 'WindowButtonUpFcn', '');
-        app.ActiveSlider = '';
-    end
